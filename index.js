977e61708efa5ca492e1cf9981ceac3ddec6cc75
Peter Kreppein <pmkreppein@gmail.com>
2019-01-03 23:05:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ function distanceTravelledInFeet (start, destination) {
 }
 function calculatesFarePrice (start, destination) {
   const distance = distanceTravelledInFeet(start, destination);
-
   if (distance <= 400) {
     return 0;
   } else if (distance > 400 && distance <= 2000) {
