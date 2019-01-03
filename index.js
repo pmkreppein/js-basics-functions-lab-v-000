6ca154fc60f5ec44ac3969e8877b192d246d41cb
Peter Kreppein <pmkreppein@gmail.com>
2019-01-03 23:09:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocknumber){
+  return blocknumber < 42 ? 42 - blocknumber : blocknumber - 42
+}
+function distanceFromHqInFeet(blocknumber) {
+  return distanceFromHqInBlocks(blocknumber) * 264
+}
+function distanceTravelledInFeet (start, destination) {
+  if (start < destination) {
+    return (destination - start) * 264
+  } else {
+    return (start - destination) * 264
+  }
+}
+function calculatesFarePrice (start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return .02 * (distance - 400)
+  } else if (distance > 2000 && distance < 2500) {
+    return 25
+  } else {
+    return 'cannot travel that far'
+  }
+}
